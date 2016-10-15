42c8304053ce9f57f021cbf862e0f414f096d97d
Maria Uretskiy <mariaur@microsoft.com>
2016-10-14 20:49:03 -0700

    Project skeleton

---
+++ PhoneNumberComparer/UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ComparerTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+    [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
